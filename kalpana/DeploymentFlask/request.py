ec5b46c0edf8dc8c8b58ed021a94b2e906d95f36
kalpanapatnala <45634823+kalpanapatnala@users.noreply.github.com>
2021-06-04 02:09:38 +0530

    Delete kalpana directory

--- kalpana/DeploymentFlask/request.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'Age':65, 'Gender':0, 'Total_Bilirubin':0.7, 'Alamine_Aminotransferase':16, 'Total_Protiens':6.8,'Albumin':3.3,'Albumin_and_Globulin_Ratio':0.9})
-
-print(r.json())
